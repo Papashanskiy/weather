ff3c6cc0e6216ecaf11f789a0731e1177fc5d270
papashanskiy <apashanskiy@gmail.com>
2020-05-03 23:01:11 +0300

    Fixed click on button bug

--- project/static/js.js
+++ project/static/js.js
@@ -19,8 +19,9 @@ $(document).ready(function () {
 
                 let out = r.result;
                 for (let i in out) {
-                    let data = out[i].toString();
-                    data = data.split(',').join(', ');
+                    // let data = out[i].toString();
+                    // data = data.split(',').join(', ');
+                    let data = out[i][1] + " " + out[i][2] + " " + out[i][3];
                     $('#searchResult').append("<div class='columns'><div class='box searchRes'>" + data + "</div></div>");
                 }
 
@@ -44,15 +45,22 @@ $(document).ready(function () {
 
         let name = $('#cityName').val();
 
-        searchCities(name);
+        if (name !== '') searchCities(name);
 
     });
 
-    $(document).on('click', '.searchRes', function (e) {
-        // alert($(this).text());
-        let btn = $(this);
-        btn.animate({height: '300px', opacity: '0.4'}, "slow");
-        btn.animate({width: '300px', opacity: '0.8'}, "slow");
+    $(document).on('mousedown', '.searchRes', function () {
+        $(this).animate({boxShadow: 'inset 2px 2px 2px'}, 'fast');
+    });
+
+    $(document).on('mouseup', '.searchRes', function () {
+        $(this)
+            .delay(100)
+            .queue(function () {
+                // $(this).css('box-shadow', '0 .5em 1em -.125em rgba(10,10,10,.1),0 0 0 1px rgba(10,10,10,.02)');
+                $(this).css('box-shadow', '');
+                $(this).dequeue();
+            });
     });
 
 });
--- project/weather/get_city.py
+++ project/weather/get_city.py
@@ -17,12 +17,12 @@ def get_city_names(s: str) -> list:
 
 def get_city_names_openweathermap(s: str) -> list:
     path = os.path.join(current_app.config['STATIC_FOLDER'], 'city.list.json')
-    with open(path, 'r') as file:
-        data = pd.DataFrame(json.loads(line) for line in file)
 
-    # df = pd.json_normalize(data)
-    print(data)
-    return []
+    df = pd.read_json(path)
+    df.dropna(inplace=True)
+
+    out = df[df['name'].str.lower().str.contains(s.lower(), na=False)]
+    return out.values.tolist()
 
 if __name__ == '__main__':
     get_city_names('Moscow')
