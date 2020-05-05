27dc532779cb4456434031e519a2c22dc162e1a5
papashanskiy <apashanskiy@gmail.com>
2020-05-05 14:45:06 +0300

    Connected server and fronend

--- project/static/js.js
+++ project/static/js.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
         searchCities(value);
     }
 
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    };
+
     function searchCities (name) {
         const url = '/api/get_user_info';
         $.ajax({
@@ -38,7 +42,33 @@ $(document).ready(function () {
     }
 
     function addToWeatherList(city, country, iconId, descript, temp, feels, min, max, windSpeed, windDeg) {
-        return true;
+        let card = "\n" +
+            "        <div class=\"columns is-multiline\">\n" +
+            "            <div class=\"column is-full\">\n" +
+            "                <div class=\"columns is-full\" style=\"width: 450px;\">\n" +
+            "                    <div class=\"column\" style=\"text-align: center;\">\n" +
+            "                        <img src=\"http://openweathermap.org/img/wn/" + iconId + "@2x.png\" style=\"width: 133px; top: -25px; position: relative;\"/>\n" +
+            "                        <p style=\"top: -45px; position: relative;\">" + descript.capitalize() + "</p>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"column\" style=\"text-align: center; font-size: 20px;\">\n" +
+            "                        <p style=\"font-size: 30px\">" + temp + " &#8451;</p>\n" +
+            "                        <p class=\"help\">Feels like " + feels + " &#8451;</p>\n" +
+            "                        <p style=\"margin-top: 20px; font-size: 15px;\">\n" +
+            "                            <img src=\"/static/wind.png\" style=\"width: 25px\">\n" +
+            "                            " + windSpeed + " km/h " + windDeg + "&#176;\n" +
+            "                        </p>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"column is-one-fifth\">\n" +
+            "                        <p style=\"color: orange;\">" + max + "</p>\n" +
+            "                        <p style=\"color: dodgerblue\">" + min + "</p>\n" +
+            "                    </div>\n" +
+            "                </div>\n" +
+            "            </div>\n" +
+            "            <div class=\"column if-full\">\n" +
+            "                <p style=\"font-size: 30px; position: relative; top: -55px; left: 30px;\">" + city + " " + country + "</p>\n" +
+            "            </div>\n" +
+            "        </div>";
+        $("#weatherCard").append(card);
     }
 
     function getWeatherById(id) {
@@ -50,13 +80,12 @@ $(document).ready(function () {
             data: {id: id},
             success: function (r) {
                 result = JSON.parse(r.result);
-                console.log(result);
                 addToWeatherList(
                     city=result.name, country=result.sys.country, iconId=result.weather[0].icon,
                     descript=result.weather[0].description, temp=result.main.temp, feels=result.main.feels_like,
                     min=result.main.temp_min, max=result.main.temp_max, windSpeed=result.wind.speed,
                     windDeg=result.wind.deg
-                    )
+                    );
             },
             error: function (xhr) {
                 alert(xhr.status);
